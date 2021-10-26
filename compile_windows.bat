d6d15c93fd82593cb629e26a4de9d2c7c7de84c0
FriwiDev <friwidev@gmail.com>
2021-10-26 15:37:47 +0200

    Remove seperate 32 bit version again

--- compile_windows.bat
+++ compile_windows.bat
@@ -8,11 +8,8 @@ if ("%2"=="") ( ^
     exit 1 ^ 
 )
 
-if "%1"=="386" (set bit=32) ^
-else (set bit=64)
-
 :: Execute build with windows Dockerfile
-docker build -t jcefbuild%bit% --file DockerfileWindows%bit% .
+docker build -t jcefbuild --file DockerfileWindows .
 
 :: Execute run with windows Dockerfile
-docker run -v jcef:c:/jcef -v out:c:/out -e TARGETARCH=%1 -e BUILD_TYPE=%2 jcefbuild%bit%
+docker run -v jcef:c:/jcef -v out:c:/out -e TARGETARCH=%1 -e BUILD_TYPE=%2 jcefbuild
