ed8466859dd39288f6a6163f2e284c1a42dcaf6e
FriwiDev <friwidev@gmail.com>
2021-10-26 10:57:50 +0200

    Try with call

--- scripts/run_windows.bat
+++ scripts/run_windows.bat
@@ -15,10 +15,28 @@ cd jcef
 mkdir jcef_build && cd jcef_build
 
 :: Load vcvars
-if "%TARGETARCH%"=="386" (cmd.exe /V /C """"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars32.bat"" "&&" "C:\builder\build_win.bat") ^
-else (cmd.exe /V /C """"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"" "&&" "C:\builder\build_win.bat")
+if "%TARGETARCH%"=="386" (call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars32.bat") ^
+else (call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat")
+
+:: Perform build
+cmake -G "Ninja" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ..
+ninja
+
+:: Compile java classes
+cd ../tools
+if "%TARGETARCH%"=="386" (compile.bat win32) else (compile.bat win64)
+
+:: Create distribution
+if "%TARGETARCH%"=="386" (make_distrib.bat win32) else (make_distrib.bat win64)
+
+:: Zip results to C:\out
+if "%TARGETARCH%"=="386" (cd ../binary_distrib/win32) else (cd ../binary_distrib/win64)
+del /F C:\out\binary_distrib.tar.gz
+tar -czvf C:\out\binary_distrib.tar.gz *
 
 GOTO :EOF
+
+
 
 :CLONE
 if exist jcef rmdir /S /Q jcef
