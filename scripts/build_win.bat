28b0c4944620eba9ac598607685934dde746685f
FriwiDev <friwidev@gmail.com>
2021-10-26 09:41:28 +0200

    Pipe rest of build to developer prompt

---
+++ scripts/build_win.bat
@@ -0,0 +1,15 @@
+:: Perform build
+cmake -G "Ninja" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ..
+ninja
+
+:: Compile java classes
+cd ../tools
+if "%TARGETARCH%"=="386" (compile.bat win32) else (compile.bat win64)
+
+:: Create distribution
+if "%TARGETARCH%"=="386" (make_distrib.bat win32) else (make_distrib.bat win64)
+
+:: Zip results to C:\out
+if "%TARGETARCH%"=="386" (cd ../binary_distrib/win32) else (cd ../binary_distrib/win64)
+del /F C:\out\binary_distrib.tar.gz
+tar -czvf C:\out\binary_distrib.tar.gz *
--- scripts/run_windows.bat
+++ scripts/run_windows.bat
@@ -15,25 +15,8 @@ cd jcef
 mkdir jcef_build && cd jcef_build
 
 :: Load vcvars
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\"
-if "%TARGETARCH%"=="386" ("C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars32.bat") ^
-else ("C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\BuildTools\Build\vcvars64.bat")
-
-:: Perform build
-cmake -G "Ninja" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ..
-ninja
-
-:: Compile java classes
-cd ../tools
-if "%TARGETARCH%"=="386" (compile.bat win32) else (compile.bat win64)
-
-:: Create distribution
-if "%TARGETARCH%"=="386" (make_distrib.bat win32) else (make_distrib.bat win64)
-
-:: Zip results to C:\out
-if "%TARGETARCH%"=="386" (cd ../binary_distrib/win32) else (cd ../binary_distrib/win64)
-del /F C:\out\binary_distrib.tar.gz
-tar -czvf C:\out\binary_distrib.tar.gz *
+if "%TARGETARCH%"=="386" ("C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars32.bat" < build_win.bat) ^
+else ("C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat" < build_win.bat)
 
 GOTO :EOF
 
