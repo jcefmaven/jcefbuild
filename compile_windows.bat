7b182f5d7447e8054bf8fa7550882f3c7216f12e
FriwiDev <friwidev@gmail.com>
2021-11-05 02:52:19 +0100

    Try with relative paths

--- compile_windows.bat
+++ compile_windows.bat
@@ -14,6 +14,6 @@ docker build -t jcefbuild --file DockerfileWindows .
 :: Execute run with windows Dockerfile
 if not exist "jcef" mkdir "jcef"
 if not exist "out" mkdir "out"
-docker run -v jcef:c:/jcef -v out:c:/out -e TARGETARCH=%1 -e BUILD_TYPE=%2 jcefbuild
+docker run -v jcef:jcef -v out:out -e TARGETARCH=%1 -e BUILD_TYPE=%2 jcefbuild
 dir jcef
 dir out
