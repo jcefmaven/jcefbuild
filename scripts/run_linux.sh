bfe55d1f9da2feb5c2c2df007e46c56f43a314e0
Fritz Windisch <friwidev@gmail.com>
2023-08-07 17:49:41 +0200

    Manually set java home

--- scripts/run_linux.sh
+++ scripts/run_linux.sh
@@ -51,7 +51,7 @@ done
 
 # Linux: Generate 32/64-bit Unix Makefiles.
 ls /usr/lib/jvm
-JAVA_HOME="/usr/lib/jvm/java-17-openjdk/" cmake -G "Ninja" -DPROJECT_ARCH=${TARGETARCH} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ..
+JAVA_HOME="/usr/lib/jvm/openjdk-17/" cmake -G "Ninja" -DPROJECT_ARCH=${TARGETARCH} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ..
 # Build native part using ninja.
 ninja -j4
 
@@ -59,7 +59,7 @@ ninja -j4
 cd ../tools
 chmod +x compile.sh
 if [ ${TARGETARCH} == 'amd64' ] || [ ${TARGETARCH} == 'arm64' ]; then
-    JAVA_HOME="/usr/lib/jvm/java-17-openjdk/" ./compile.sh linux64
+    JAVA_HOME="/usr/lib/jvm/openjdk-17/" ./compile.sh linux64
 elif [ ${TARGETARCH} == '386' ]; then
     echo "386 is no longer supported since chromium 104"
     exit 1
@@ -75,9 +75,9 @@ set -e
 #Generate distribution
 chmod +x make_distrib.sh
 if [ ${TARGETARCH} == 'amd64' ] || [ ${TARGETARCH} == 'arm64' ]; then
-    JAVA_HOME="/usr/lib/jvm/java-17-openjdk/" ./make_distrib.sh linux64
+    JAVA_HOME="/usr/lib/jvm/openjdk-17/" ./make_distrib.sh linux64
 else
-    JAVA_HOME="/usr/lib/jvm/java-17-openjdk/" ./make_distrib.sh linux32
+    JAVA_HOME="/usr/lib/jvm/openjdk-17/" ./make_distrib.sh linux32
 fi
 
 #Pack binary_distrib
