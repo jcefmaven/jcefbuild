ff5fc9020a45e5868d558c0adaaa66b073ea05a8
Friwi <FriwiDev@users.noreply.github.com>
2023-12-19 11:18:10 +0100

    Update install_macos_dependencies.sh

--- scripts/install_macos_dependencies.sh
+++ scripts/install_macos_dependencies.sh
@@ -2,3 +2,6 @@
 
 brew install ninja
 python3 -m pip install --ignore-installed six
+
+/Library/Frameworks/Python.framework/Versions/Current/bin/python -m pip list
+echo "import six.moves" | /Library/Frameworks/Python.framework/Versions/Current/bin/python
