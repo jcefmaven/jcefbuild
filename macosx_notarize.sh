2a837a2972d27218a3d223c07a00fa06b80268ce
Friwi <FriwiDev@users.noreply.github.com>
2024-01-16 17:03:25 +0100

    Update macosx_notarize.sh

--- macosx_notarize.sh
+++ macosx_notarize.sh
@@ -49,7 +49,8 @@ rm -f notarization.log
 echo ""
 
 # staple
-mkdir -p "$1/Contents/CodeResources" || true
+mkdir -p "$1/Contents" || true
+ln -s -r "$1/Contents/CodeResources" "$1/_CodeSignature/CodeResources"
 xcrun stapler staple -v "$1"
 
 echo "##########################################################"
