af60c79e48bec5d539cfa287943e419474591207
Friwi <FriwiDev@users.noreply.github.com>
2024-01-16 10:36:10 +0100

    Roll back python compiler fixing

--- scripts/install_macos_dependencies.sh
+++ scripts/install_macos_dependencies.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-PYTHON=/usr/local/Frameworks/Python.framework/Versions/3.12/bin/python3.12
-
 brew install ninja
-"$PYTHON" -m pip install --ignore-installed six
-
-"$PYTHON" -m pip list
-echo "from six.moves import configparser" | "$PYTHON" && echo "Success importing stuff from six moves python module"
