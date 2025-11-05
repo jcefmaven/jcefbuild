0e3a94c5bff9374cc11e850887842dbb49d0abcd
Friwi <FriwiDev@users.noreply.github.com>
2025-11-05 08:06:16 +0100

    Update install_macos_dependencies.sh

--- scripts/install_macos_dependencies.sh
+++ scripts/install_macos_dependencies.sh
@@ -3,7 +3,7 @@
 PYTHON=python3
 
 brew install ninja
-brew install six
+pip3 install six
 
 "$PYTHON" -m pip list
 echo "from six.moves import configparser" | "$PYTHON" && echo "Success importing stuff from six moves python module"
